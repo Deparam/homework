8ee8eef308d5e0311486cccbe8e6e6061709d57c
Deparam <55419722+Deparam@users.noreply.github.com>
2020-03-14 13:39:40 -0400

    commiting

---
+++ Web_Scrapping/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import pymongo
+import scrape_mars
+
+app = Flask(__name__)
+
+@app.route("/scrape")
+def execute_scrape():
+    return scrape_mars.scrape()
+
+@app.route("/")
+def render_html():
+    # extract data from local mongodb 
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["mars_db"] 
+    mars_collection = mydb['mars_collection']
+    mars_data = mars_collection.find_one()  
+
+    # render html 
+    return render_template("index.html", mars_data=mars_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
